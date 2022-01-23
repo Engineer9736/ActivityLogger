954bd715292e8677136b5cafc58044c29a77f447
Engineer9736 <richard@rscheib.eu>
2022-01-23 17:41:40 +0100

    dev

--- ActivityLogger/src/engineer/Main.java
+++ ActivityLogger/src/engineer/Main.java
@@ -12,11 +12,13 @@ import java.nio.file.StandardOpenOption;
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.*;
+import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
 
 public class Main implements NativeKeyListener {
 	
 	private Integer keyboardStrokesLogged = 0;
 	private Integer mouseMovesLogged = 0;
+	private Integer lastFileWrite = 0;
 	
 	private Path file = Paths.get("activity.txt");
 
@@ -32,15 +34,40 @@ public class Main implements NativeKeyListener {
 	}
 
 	public void nativeKeyPressed(NativeKeyEvent e) {
+		keyboardStrokesLogged++;
 		
+		checkIfFileWriteDue();
+	}
+	
+	public void nativeMouseMoved(NativeMouseEvent e) {
+		mouseMovesLogged++;
+		
+		checkIfFileWriteDue();
+	}
+	
+	private void checkIfFileWriteDue() {
+		
+		Integer unixTime = (int) (System.currentTimeMillis() / 1000);
+		
+		if (lastFileWrite + 1 < unixTime) { // Last file write was 5 minutes ago.
+			
+			// Record the current time as lastFileWrite.
+			lastFileWrite = unixTime;
+			
+			// Write the current logged data to file and reset the counters.
+			writeResultsToFileAndReset();
+		}
 	}
 	
 	private void writeResultsToFileAndReset() {		
 		try (OutputStream os = Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
 				StandardOpenOption.APPEND); PrintWriter writer = new PrintWriter(os)) {
 			
-				writer.print("[" + java.time.LocalDate.now() + "]");
-			
+				writer.println("[" + java.time.LocalDate.now() + "] " + keyboardStrokesLogged + " keystrokes, " + mouseMovesLogged + " mousemoves.");
+				
+				keyboardStrokesLogged = 0;
+				mouseMovesLogged = 0;
+				
 		} catch (IOException ex) {
 			//
 		}
