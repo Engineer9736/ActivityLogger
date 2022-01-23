49acdcecdf10a5ee291a37e3363f3f3d0d069633
Engineer9736 <richard@rscheib.eu>
2022-01-23 17:59:14 +0100

    dev

--- ActivityLogger/src/engineer/Main.java
+++ ActivityLogger/src/engineer/Main.java
@@ -8,13 +8,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.*;
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
+import com.github.kwhat.jnativehook.mouse.NativeMouseMotionListener;
 
-public class Main implements NativeKeyListener {
+public class Main implements NativeKeyListener, NativeMouseMotionListener {
 	
 	private Integer keyboardStrokesLogged = 0;
 	private Integer mouseMovesLogged = 0;
@@ -30,7 +35,10 @@ public class Main implements NativeKeyListener {
 			//
 		}
 		
-		GlobalScreen.addNativeKeyListener(new Main());
+		Main ourMain = new Main();
+		
+		GlobalScreen.addNativeKeyListener(ourMain);
+		GlobalScreen.addNativeMouseMotionListener(ourMain);
 	}
 
 	public void nativeKeyPressed(NativeKeyEvent e) {
@@ -49,7 +57,7 @@ public class Main implements NativeKeyListener {
 		
 		Integer unixTime = (int) (System.currentTimeMillis() / 1000);
 		
-		if (lastFileWrite + 1 < unixTime) { // Last file write was 5 minutes ago.
+		if (lastFileWrite + 60*5 < unixTime) { // Last file write was 5 minutes ago.
 			
 			// Record the current time as lastFileWrite.
 			lastFileWrite = unixTime;
@@ -59,11 +67,26 @@ public class Main implements NativeKeyListener {
 		}
 	}
 	
-	private void writeResultsToFileAndReset() {		
+	private void writeResultsToFileAndReset() {
 		try (OutputStream os = Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
 				StandardOpenOption.APPEND); PrintWriter writer = new PrintWriter(os)) {
 			
-				writer.println("[" + java.time.LocalDate.now() + "] " + keyboardStrokesLogged + " keystrokes, " + mouseMovesLogged + " mousemoves.");
+			
+				String pattern = "dd-MM-yyyy HH:mm:ss";
+	
+				// Create an instance of SimpleDateFormat used for formatting 
+				// the string representation of date according to the chosen pattern
+				DateFormat df = new SimpleDateFormat(pattern);
+	
+				// Get the today date using Calendar object.
+				java.util.Date today = Calendar.getInstance().getTime();        
+				// Using DateFormat format method we can create a string 
+				// representation of a date with the defined format.
+				String todayAsString = df.format(today);
+			
+			
+			
+				writer.println("[" + todayAsString + "] " + keyboardStrokesLogged + " keystrokes, " + mouseMovesLogged + " mousemoves.");
 				
 				keyboardStrokesLogged = 0;
 				mouseMovesLogged = 0;
